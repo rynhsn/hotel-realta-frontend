51276338cadd70d466fdbbd185d9e84d064af40a
Riyan Husen <riyan.husen@gmail.com>
2023-03-09 19:54:10 +0700

    fresh

--- Realta.Frontend/HttpRepository/Hotel/Hotels/IHotelsHttpRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Realta.Contract.Models.v1.Hotels;
-
-namespace Realta.Frontend.HttpRepository
-{
-    public interface IHotelsHttpRepository
-    {
-        Task<List<HotelsDto>> GetHotels();
-    }
-}
