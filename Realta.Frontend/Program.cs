c49aff73be9f45b6f1e496fdada1e98c13eea9c8
Riyan Husen <riyan.husen@gmail.com>
2023-03-09 12:56:34 +0700

    add readme file

--- Realta.Frontend/Program.cs
+++ Realta.Frontend/Program.cs
@@ -9,6 +9,24 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7068/api/") });
+
+// booking
+
+// hotel
+
+// hr
+
+// master
+
+// payment
+
+// purchasing
 builder.Services.AddScoped<IPurchaseOrderHttpRepository, PurchaseOrderHttpRepository>();
+
+// resto
+
+// users
+
+
 
 await builder.Build().RunAsync();
